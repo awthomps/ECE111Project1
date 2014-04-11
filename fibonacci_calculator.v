69c1c83ee77a3e8a0337dbeaa488a100f6677527
Andrei Thompson <andreiwthomps@gmail.com>
2014-04-11 10:54:58 -0700

    Added files after initial commit.

---
+++ fibonacci_calculator.v
@@ -0,0 +1,13 @@
+module fibonacci_calculator(input_s, reset_n, begin_fibo, clk, done, fibo_out);
+	input [4:0] input_s;
+	input reset_n;
+	input begin_fibo; //Start calculation
+	input clk;
+	
+	output done;
+	output [15:0] fibo_out;
+	
+	
+
+
+endmodule
